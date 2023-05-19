b48eec78dadf9adb482d159b5bc68b36eaae8c8d
rubbishasf <105477992+rubbishasf@users.noreply.github.com>
2023-05-19 22:12:17 +0800

    add simulator test

--- MIPS.cpp
+++ MIPS.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <assert.h>
 #include "Anti_assembler.cpp"
+#include "mipssimulator.cpp"
 
 using namespace std;
 
@@ -25,29 +26,6 @@ using namespace std;
 //32个寄存器
 
 
-class MIPS_Simulator
-{
-public:
-    void Load();   //将uint32位指令读入内存
-    void Execute();  //执行指令
-    /*
-    接口也不一定长这样，怎么好写怎么来
-    */
-private:
-    //PC等寄存器,内存（可用数组实现）
-    //其它寄存器可以参考如下实现
-    const unordered_map<string, unsigned int> REGISTERS = 
-    {
-        {"$zero", 0},{"$at", 1},{"$v0", 2},{"$v1", 3},
-        {"$a0", 4},{"$a1", 5},{"$a2", 6},{"$a3", 7},
-        {"$t0", 8},{"$t1", 9},{"$t2", 10},{"$t3", 11},
-        {"$t4", 12},{"$t5", 13},{"$t6", 14},{"$t7", 15},
-        {"$s0", 16},{"$s1", 17},{"$s2", 18},{"$s3", 19},
-        {"$s4", 20},{"$s5", 21},{"$s6", 22},{"$s7", 23},
-        {"$t8", 24}, {"$t9", 25},{"$k0", 26},{"$k1", 27},
-        {"$gp", 28},{"$sp", 29},{"$fp", 30},{"$ra", 31}
-    };
-};
 
 //汇编
 string Assemble(string instruction);
@@ -60,7 +38,7 @@ string Assemble(string instruction);
 Anti_assembler class:
     Need to Initialize before you use it.
 */
-Anti_assembler anti_assembler;
+//Anti_assembler anti_assembler;
 
 /*
 Test for Disassembler:
@@ -111,6 +89,26 @@ void test_for_Disassembler()
     printf("\nTest Successfully!\n");
 }
 
+void test_for_Simulator(){
+
+    vector<string> input = {
+        "00000001001010100100000000100000",
+        "00000001001010100100000000100010",
+        "00100001001010000000000000000001",
+        "10001101001010000000000000000100",
+        "00010001000010010000000111110100",
+        "00010101000010010000000111110100",
+        "00001100000000000000111110100000",
+        "00000011111000000000000000000100",
+        "00000001001010100100000000101010",
+        "10101101001010001111111111111100"
+    };
+    MipsSimulator mips;
+    mips.load(input);
+    mips.Execute();
+    mips.Showallreg();
+}
+
 
 int main()
 {
@@ -127,5 +125,6 @@ int main()
     // string instr_jr = Assemble("jr $ra");
     // string instr_slt = Assemble("slt $s0, $s1, $s2");
     test_for_Disassembler();
+    test_for_Simulator();
     return 0;
 }
